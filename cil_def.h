2e215b3b8056031c907710b56b5ab33639eed797
Shuangquan Li <lishq991@gmail.com>
2016-07-30 14:53:11 +0800

    Update cil_def.h

--- cil_def.h
+++ cil_def.h
@@ -49,98 +49,98 @@
 #include <type_traits>
 #endif
 
-
-#define FOR(i, a, b)          for (long long i = (long long)(a); i < (long long)(b); ++i)
-#define ROF(i, a, b)          for (long long i = (long long)(b) - 1; i >= (long long)(a); --i)
-#define rep(i, a, b)          for (long long i = (long long)(a); i < (long long)(b); ++i)
-#define per(i, a, b)          for (long long i = (long long)(b) - 1; i >= (long long)(a); --i)
-#define REP(i, n)             for (long long i = 0; i < (long long)(n); ++i)
-#define PER(i, n)             for (long long i = (long long)(n) - 1; i >= 0; --i)
-#if                           defined(__GNUC__) || defined(__GNUG__)
-#define ECH(i, c)             for (__typeof(c.begin()) i = c.begin(), e = c.end(); i != e; ++i)
-#define HCE(i, c)             for (__typeof(c.rbegin()) i = c.rbegin(), e = c.rend(); i != e; ++i)
+#define FOR(i, a, b)                 for (long long i = (long long)(a); i < (long long)(b); ++i)
+#define ROF(i, a, b)                 for (long long i = (long long)(b) - 1; i >= (long long)(a); --i)
+#define rep(i, a, b)                 for (long long i = (long long)(a); i < (long long)(b); ++i)
+#define per(i, a, b)                 for (long long i = (long long)(b) - 1; i >= (long long)(a); --i)
+#define REP(i, n)                    for (long long i = 0; i < (long long)(n); ++i)
+#define PER(i, n)                    for (long long i = (long long)(n) - 1; i >= 0; --i)
+#if                                  defined(__GNUC__) || defined(__GNUG__)
+#define ECH(i, c)                    for (__typeof(c.begin()) i = c.begin(), e = c.end(); i != e; ++i)
+#define HCE(i, c)                    for (__typeof(c.rbegin()) i = c.rbegin(), e = c.rend(); i != e; ++i)
 #else
-#define ECH(i, c)             for (auto i = c.begin(), e = c.end(); i != e; ++i)
-#define HCE(i, c)             for (auto i = c.rbegin(), e = c.rend(); i != e; ++i)
+#define ECH(i, c)                    for (auto i = c.begin(), e = c.end(); i != e; ++i)
+#define HCE(i, c)                    for (auto i = c.rbegin(), e = c.rend(); i != e; ++i)
 #endif
-#define DO(n)                 for (int _iDO_ = int(n); _iDO_--;)
-#define rush()                int _CASES_; scanf("%d", &_CASES_); for (; _CASES_--;)
-#define cases()               int _CASES_; scanf("%d", &_CASES_); for (int iCase = 1; (iCase <= _CASES_ && printf("Case #%d:\n", iCase)); ++iCase)
-#define sqr(x)                ((x) * (x))
-#define cub(x)                ((x) * (x) * (x))
-#define all(x)                (x).begin(), (x).end()
-#define sortall(x)            sort((x).begin(), (x).end())
-#define reverseall(x)         reverse((x).begin(), (x).end())
-#define sz(x)                 ((int)(x).size())
-#define eb                    emplace_back
-#define pb                    push_back
-#define pf                    push_front
-#define ppb                   pop_back
-#define ppf                   pop_front
-#define mp                    make_pair
-#define pbmp(a, b)            push_back(make_pair((a), (b)))
-#define fi                    first
-#define se                    second
-#define ms(a, b)              memset((a), (b), sizeof(a))
-#define cp(a, b)              memcpy((a), (b), sizeof(a))
-#define IOSS0                 std::ios::sync_with_stdio(false);cin.tie(false);
-#define whatis(x)             cout << #x << " = " << x << endl;
-#define uniqueit(v)           (v).erase(unique((v).begin(), (v).end()), (v).end())
-#define insertlb(v, a)        (v).insert(lower_bound((v).begin(), (v).end(), (a)), (a))
-#define insertub(v, a)        (v).insert(upper_bound((v).begin(), (v).end(), (a)), (a))
-#define PQ                    priority_queue
-#define uset                  unordered_set
-#define mset                  multiset
-#define umap                  unordered_map
-#define mmap                  multimap
-typedef double                db;
-typedef long double           ldb;
-typedef unsigned char         uchar;
-typedef unsigned short        ushort;
-typedef unsigned int          uint;
-typedef long long             ll;
-typedef unsigned long long    ull;
-typedef vector<bool>          VB;
-typedef vector<VB>            VVB;
-typedef vector<char>          VC;
-typedef vector<VC>            VVC;
-typedef vector<int>           VI;
-typedef vector<VI>            VVI;
-typedef vector<ll>            VLL;
-typedef vector<VLL>           VVLL;
-typedef vector<double>        VD;
-typedef vector<VD>            VVD;
-typedef vector<string>        VS;
-typedef vector<VS>            VVS;
-typedef pair<int, int>        PII;
-typedef pair<ll, ll>          PLL;
-typedef vector<PII>           VPII;
-typedef vector<PLL>           VPLL;
-typedef pair<int, PII>        PIPII;
-typedef pair<ll, PLL>         PLPLL;
-typedef pair<PII, int>        PPIII;
-typedef pair<PLL, ll>         PPLLL;
-typedef map<int, int>         MII;
-typedef map<ll, ll>           MLL;
-typedef map<db, db>           MDD;
-typedef map<string, int>      MSI;
-typedef map<int, string>      MIS;
-typedef set<int>              SI;
-typedef set<ll>               SLL;
-typedef queue<int>            QI;
-typedef queue<ll>             QLL;
-typedef queue<PII>            QPII;
-typedef deque<int>            DI;
-typedef deque<ll>             DLL;
-typedef deque<PII>            DPII;
-const double                  eps = 1e-9;
-const double                  pi = 3.14159265358979323846264338327950288419716939937510;
-const int                     inf = 0x3f3f3f3f;
-const int                     mod = 1000000007;
-const int                     mod2 = 1000000006;
-const long long               infll = 0x3f3f3f3f3f3f3f3fLL;
-const int                     dx[8] = { 0, 1, 0, -1, -1, 1, 1, -1 };
-const int                     dy[8] = { 1, 0, -1, 0, 1, 1, -1, -1 };
+#define DO(n)                        for (int _iDO_ = int(n); _iDO_--;)
+#define rush()                       int _CASES_; scanf("%d", &_CASES_); for (; _CASES_--;)
+#define cases()                             int _CASES_; scanf("%d", &_CASES_); for (int iCase = 1; (iCase <= _CASES_ && printf("Case #%d:\n", iCase)); ++iCase)
+#define sqr(x)                       ((x) * (x))
+#define cub(x)                       ((x) * (x) * (x))
+#define all(x)                       (x).begin(), (x).end()
+#define sortall(x)                   sort((x).begin(), (x).end())
+#define reverseall(x)                reverse((x).begin(), (x).end())
+#define sz(x)                        ((int)(x).size())
+#define eb                           emplace_back
+#define pb                           push_back
+#define pf                           push_front
+#define ppb                          pop_back
+#define ppf                          pop_front
+#define mp                           make_pair
+#define pbmp(a, b)                   push_back(make_pair((a), (b)))
+#define fi                           first
+#define se                           second
+#define ms(a, b)                     memset((a), (b), sizeof(a))
+#define cp(a, b)                     memcpy((a), (b), sizeof(a))
+#define IOSS0                        std::ios::sync_with_stdio(false);cin.tie(false);
+#define whatis(x)                    cout << #x << " = " << x << endl;
+#define uniqueit(v)                  (v).erase(unique((v).begin(), (v).end()), (v).end())
+#define insertlb(v, a)               (v).insert(lower_bound((v).begin(), (v).end(), (a)), (a))
+#define insertub(v, a)               (v).insert(upper_bound((v).begin(), (v).end(), (a)), (a))
+#define PQ                           priority_queue
+#define uset                         unordered_set
+#define mset                         multiset
+#define umap                         unordered_map
+#define mmap                         multimap
+typedef double                       db;
+typedef long double                  ldb;
+typedef unsigned char                uchar;
+typedef unsigned short               ushort;
+typedef unsigned int                 uint;
+typedef long long                    ll;
+typedef unsigned long long           ull;
+typedef std::vector<bool>            VB;
+typedef std::vector<VB>              VVB;
+typedef std::vector<char>            VC;
+typedef std::vector<VC>              VVC;
+typedef std::vector<int>             VI;
+typedef std::vector<VI>              VVI;
+typedef std::vector<ll>              VLL;
+typedef std::vector<VLL>             VVLL;
+typedef std::vector<double>          VD;
+typedef std::vector<VD>              VVD;
+typedef std::vector<std::string>     VS;
+typedef std::vector<VS>              VVS;
+typedef std::pair<int, int>          PII;
+typedef std::pair<ll, ll>            PLL;
+typedef std::vector<PII>             VPII;
+typedef std::vector<PLL>             VPLL;
+typedef std::pair<int, PII>          PIPII;
+typedef std::pair<ll, PLL>           PLPLL;
+typedef std::pair<PII, int>          PPIII;
+typedef std::pair<PLL, ll>           PPLLL;
+typedef std::map<int, int>           MII;
+typedef std::map<ll, ll>             MLL;
+typedef std::map<db, db>             MDD;
+typedef std::map<std::string, int>   MSI;
+typedef std::map<int, std::string>   MIS;
+typedef std::set<int>                SI;
+typedef std::set<ll>                 SLL;
+typedef std::queue<int>              QI;
+typedef std::queue<ll>               QLL;
+typedef std::queue<PII>              QPII;
+typedef std::deque<int>              DI;
+typedef std::deque<ll>               DLL;
+typedef std::deque<PII>              DPII;
+const double                         eps = 1e-9;
+const double                         pi = 3.14159265358979323846264338327950288419716939937510;
+const int                            inf = 0x3f3f3f3f;
+const int                            mod = 1000000007;
+const int                            mod2 = 1000000006;
+const long long                      infll = 0x3f3f3f3f3f3f3f3fLL;
+const int                            dx[8] = { 0, 1, 0, -1, -1, 1, 1, -1 };
+const int                            dy[8] = { 1, 0, -1, 0, 1, 1, -1, -1 };
+
 
 
 /* eof */
