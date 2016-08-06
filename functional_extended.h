d6ac8c9319d477507a5341e69c436957c2acd6c0
Shuangquan Li <lishq991@gmail.com>
2016-08-06 18:34:28 +0800

    Add files via upload

---
+++ functional_extended.h
@@ -0,0 +1,40 @@
+/*
+* functional-extended header.
+* written by Shuangquan Li, lishq991@gmail.com
+* created on 2016-8-6
+*/
+
+#ifndef __FUNCTIONAL_EXTENDED_H__
+#define __FUNCTIONAL_EXTENDED_H__
+
+#include <vector>
+#include <functional>
+
+
+template<typename T, T mod>
+struct plus_modulus : std::binary_function<T, T, T> {
+	T operator () (const T& a, const T& b) const {
+		T ret = (a + b) % mod;
+		if (ret < 0) ret += mod;
+		return ret;
+	}
+};
+
+template<typename T, T mod>
+struct minus_modulus : std::binary_function<T, T, T> {
+	T operator () (const T& a, const T& b) {
+		T ret = (a - b) % mod;
+		if (ret < 0) ret += mod;
+		return ret;
+	}
+};
+
+template<typename T, T mod>
+struct multiplies_modulus : std::binary_function<T, T, T> {
+	T operator () (const T& a, const T& b) {
+		return a * b % mod;
+	}
+};
+
+/* eof */
+#endif
