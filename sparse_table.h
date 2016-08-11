3697b8233a0c2199c58a2963d5fd04a397077e4e
Shuangquan Li <lishq991@gmail.com>
2016-08-11 21:36:42 +0800

    Add files via upload

---
+++ sparse_table.h
@@ -0,0 +1,73 @@
+/*
+ * sparse-table header.
+ * written by Shuangquan Li, lishq991@gmail.com
+ * created on 2016-8-11
+ */
+
+#ifndef __SPARSE_TABLE__
+#define __SPARSE_TABLE__
+
+#include <iostream>
+#include <vector>
+
+template<typename T, typename Operator>
+class sparse_table {
+	std::vector<std::vector<T>> __st;
+	Operator __op;
+public:
+	sparse_table() {}
+	template<typename ForwardIterator>
+	sparse_table(ForwardIterator first, ForwardIterator last) { init(first, last); }
+	sparse_table(const sparse_table& rhs) { __st = rhs.__st; __op = rhs.__op; }
+	template<typename ForwardIterator>
+	void init(ForwardIterator first, ForwardIterator last) {
+		__st.clear();
+		for (ForwardIterator it = first; it != last; ++it) {
+			vector<T> tmp;
+			tmp.push_back(*it);
+			__st.push_back(tmp);
+		}
+		int n = (int)(__st.size());
+		for (int j = 1; (1 << j) <= n; ++j)
+			for (int i = 0; i + (1 << j) <= n; ++i)
+				__st[i].push_back(__op(__st[i][j - 1], __st[i + (1 << (j - 1))][j - 1]));
+	}
+	void add(const T& v) {
+		std::vector<T> tmp;
+		tmp.push_back(v);
+		__st.push_back(tmp);
+		int j = 1;
+		int r = int(__st.size());
+		while (true) {
+			int i = r - (1 << j);
+			if (i < 0) break;
+			__st[i].push_back(__op(st[i][j - 1], __st[i + (1 << (j - 1))][j - 1]));
+			++j;
+		}
+	}
+	// query interval [l, r]
+	T query(int l, int r) {
+		int d = r - l + 1;
+		int j = 0;
+		while ((1 << (j + 1)) <= d) ++j;
+		return __op(__st[l][j], __st[r - (1 << j) + 1][j]);
+	}
+	void clear() { __st.clear(); }
+	size_t size() const { return __st.size(); }
+	const vector<T>& operator[](int i) const { return __st[i]; }
+};
+
+template<typename T, typename Compare>
+std::ostream& operator<< (std::ostream& os, const sparse_table<T, Compare>& st) {
+	for (int i = 0; i < int(st.size()); ++i) {
+		os << i << ":";
+		for (int j = 0; j < int(st[i].size()); ++j)
+			os << " " << st[i][j];
+		os << std::endl;
+	}
+	return os;
+}
+
+
+/* eof */
+#endif
