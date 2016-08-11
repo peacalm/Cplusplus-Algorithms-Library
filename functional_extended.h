b88f6062117c1ed323d991889eed88442df5c170
Shuangquan Li <lishq991@gmail.com>
2016-08-11 21:38:26 +0800

    Update functional_extended.h

--- functional_extended.h
+++ functional_extended.h
@@ -2,6 +2,7 @@
  * functional-extended header.
  * written by Shuangquan Li, lishq991@gmail.com
  * created on 2016-8-6
+ * last edit on 2016-8-11, add maxone, minone
  */
 
 #ifndef __FUNCTIONAL_EXTENDED_H__
@@ -10,6 +11,19 @@
 #include <vector>
 #include <functional>
 
+template<typename T, typename Compare = std::less<T>>
+struct maxone : std::binary_function<T, T, T> {
+	T operator() (const T& a, const T& b) {
+		return Compare()(a, b) ? b : a;
+	}
+};
+
+template<typename T, typename Compare = std::less<T>>
+struct minone : std::binary_function<T, T, T> {
+	T operator() (const T& a, const T& b) {
+		return Compare()(a, b) ? a : b;
+	}
+};
 
 template<typename T, T mod>
 struct plus_modulus : std::binary_function<T, T, T> {
