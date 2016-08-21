8c71097ece7d52911536a417c1c574eb6294a0d5
Shuangquan Li <lishq991@gmail.com>
2016-08-22 01:31:04 +0800

    Update treap_tree.h

--- treap_tree.h
+++ treap_tree.h
@@ -1,8 +1,8 @@
 /*
-* treap-tree header.
-* written by Shuangquan Li, lishq991@gmail.com
-* created on 2016-8-20
-*/
+ * treap-tree header.
+ * written by Shuangquan Li, lishq991@gmail.com
+ * created on 2016-8-20
+ */
 
 #ifndef __TREAP_TREE_H__
 #define __TREAP_TREE_H__
@@ -10,15 +10,22 @@
 #include <cstdlib>
 #include <iostream>
 
-template<typename T>
+struct default_random_weight_generator {
+	int operator()() const {
+		return rand() << 15 | rand();
+	}
+};
+
+template<typename T, typename RandomWeightGenerator = default_random_weight_generator>
 struct treap_node_base {
 	T val;
 	int weight;
 	size_t subtree_size;
 	treap_node_base* father, *left, *right;
+	static RandomWeightGenerator generate_weight;
 	treap_node_base(T v = 0) : val(v), weight(generate_weight()), subtree_size(1), father(0), left(0), right(0) {};
-	static int generate_weight();
-	virtual ~treap_node_base() {}
+	//virtual ~treap_node_base() {}
+	virtual void destroy() {}
 	virtual void downpush() {}
 	void print() {
 		printf("val = %d\nweight = %d\n", val, weight);
@@ -35,10 +42,8 @@ struct treap_node_base {
 		OUT();
 	}
 };
-template<typename T>
-int treap_node_base<T>::generate_weight() {
-	return rand() << 15 | rand();
-}
+template<typename T, typename RandomWeightGenerator = default_random_weight_generator>
+RandomWeightGenerator treap_node_base<T, RandomWeightGenerator>::generate_weight = RandomWeightGenerator();
 
 
 //
@@ -48,27 +53,70 @@ private:
 	TreapNode* __root;
 public:
 	treap_tree(TreapNode* r = 0) : __root(r){}
-	size_t size() const { return __root->subtree_size; }
-	TreapNode* insert(T newval) {
-		TreapNode* newnode = new TreapNode(newval);
+	size_t size() const { return __root ? __root->subtree_size : 0; }
+	TreapNode* insert(const T& val) {
+		TreapNode* newnode = new TreapNode(val);
 		return insert(newnode);
 	}
 	TreapNode* insert(TreapNode* newnode) {
-		OUT("insert ", newnode->val);
 		if (!__root) {
 			__root = newnode;
-			newnode->print();
 			return newnode;
 		}
 		__insert(__root, newnode);
 		__rotate(newnode);
-		newnode->print();
 		return newnode;
+	}
+	TreapNode* find(const T& val) {
+		return __find(__root, val);
+	}
+	void erase(const T& val) {
+		return erase(__find(__root, val));
+	}
+	void erase(TreapNode* node) {
+		while (node->left && node->right) {
+			if (node->left->weight < node->right->weight) __right_rotate(node);
+			else __left_rotate(node);
+		}
+		TreapNode *f = node->father;
+		while (f) {
+			--(f->subtree_size);
+			f = f->father;
+		}
+		if (node->left) {
+			node->left->father = node->father;
+			if (node == __root) __root = node->left;
+			else {
+				if (node == node->father->left) node->father->left = node->left;
+				else node->father->right = node->left;
+			}
+		}
+		else if (node->right) {
+			node->right->father = node->father;
+			if (node == __root) __root = node->right;
+			else {
+				if (node == node->father->left) node->father->left = node->right;
+				else node->father->right = node->right;
+			}
+		}
+		else {
+			if (node == __root) __root = NULL;
+			else {
+				if (node == node->father->left) node->father->left = NULL;
+				else node->father->right = NULL;
+			}
+		}
+		node->destroy();
+		delete node;
 	}
 	std::pair<treap_tree, treap_tree> split() {
 
 	}
+	treap_tree merge(treap_tree& l, treap_tree& r) {
+
+	}
 	void traversal() {
+		OUT("traversal, root =", __root->val);
 		dfs(__root);
 	}
 	void dfs(TreapNode *u){
@@ -79,22 +127,23 @@ public:
 		dfs(u->right);
 	}
 private:
+	TreapNode* __find(TreapNode* u, const T& val) {
+		if (!u) return u;
+		if (u->val < val) return __find(u->right, val);
+		if (u->val > val) return __find(u->left, val);
+		return u;
+	}
 	void __rotate(TreapNode* newnode) {
 		while (newnode->father) {
 			TreapNode* f = newnode->father;
-			std::cout << std::endl;
-			if (newnode->weight < f->weight) {
-				if (newnode == (f->left) )
-					__right_rotate(f);
-				else 
-					__left_rotate(f);
-			}
-			else
-				break;
+			if (newnode->weight >= f->weight) break;
+			if (newnode == (f->left)) __right_rotate(f);
+			else __left_rotate(f);
 		}
 	}
 	TreapNode* __insert(TreapNode* cur, TreapNode* newnode) {
 		++(cur->subtree_size);
+		cur->downpush();
 		if (newnode->val < cur->val) {
 			if (cur->left != 0)
 				return __insert(cur->left, newnode);
