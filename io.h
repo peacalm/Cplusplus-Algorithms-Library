296286cfb14a1598026a4151c6be3071238dd876
lishuangquan <lishuangquan@bytedance.com>
2018-08-24 20:39:53 +0800

    io: keep only cpp11 edition
    Change-Id: Iffc48c0244598c891e467e481bb8ffca1a6732cf

--- io.h
+++ io.h
@@ -11,141 +11,42 @@
 #include <cstdio>
 #include <iostream>
 
-// integer read and write
-
 // read integer types
 template<typename T> inline bool RD(T& v) { char c; bool n; while (c = getchar(), c != '-' && (c < '0' || c > '9')) if (c == EOF) return false; if (c == '-') { n = true; v = 0; } else { n = false; v = c - '0'; } while (c = getchar(), c >= '0' && c <= '9') v = (v << 3) + (v << 1) + c - '0'; if (n) v *= -1; return true; }
-#ifdef __cpp11
 template<typename A, typename ...Args> inline bool RD(A& a, Args&... rest) { return RD(a) && RD(rest...); }
-#else
-template<typename T> inline bool RD(T& a, T& b) { return RD(a) && RD(b); }
-template<typename T> inline bool RD(T& a, T& b, T& c) { return RD(a, b) && RD(c); }
-template<typename T> inline bool RD(T& a, T& b, T& c, T& d) { return RD(a, b, c) && RD(d); }
-template<typename T> inline bool RD(T& a, T& b, T& c, T& d, T& e) { return RD(a, b, c, d) && RD(e); }
-template<typename T> inline bool RD(T& a, T& b, T& c, T& d, T& e, T& f) { return RD(a, b, c, d, e) && RD(f); }
-template<typename T> inline bool RD(T& a, T& b, T& c, T& d, T& e, T& f, T& g) { return RD(a, b, c, d, e, f) && RD(g); }
-template<typename T> inline bool RD(T& a, T& b, T& c, T& d, T& e, T& f, T& g, T& h) { return RD(a, b, c, d, e, f, g) && RD(h); }
-template<typename T> inline bool RD(T& a, T& b, T& c, T& d, T& e, T& f, T& g, T& h, T& i) { return RD(a, b, c, d, e, f, g, h) && RD(i); }
-template<typename T> inline bool RD(T& a, T& b, T& c, T& d, T& e, T& f, T& g, T& h, T& i, T& j) { return RD(a, b, c, d, e, f, g, h, i) && RD(j); }
-#endif
 inline int RD() { int v; RD(v); return v; }
+#define RDn(a, l, r)          { for (int _i_ = l; _i_ < r; ++_i_) RD((a)[_i_]); }
+#define RDnm(a, b, c, d, e)   { for (int __i__ = b; __i__ < c; ++__i__) RDn(a[__i__], d, e) }
+
 // read integer types with buffer
 inline char getchar_buffered() { static char _BUF_[1 << 15], *_HEAD_ = _BUF_, *_TAIL_ = _BUF_; return _HEAD_ == _TAIL_ && (_TAIL_ = (_HEAD_ = _BUF_) + fread(_BUF_, 1, 1 << 15, stdin), _HEAD_ == _TAIL_) ? EOF : *_HEAD_++; }
 template<typename T> inline bool RDB(T& v) { char c; bool n; while (c = getchar_buffered(), c != '-' && (c < '0' || c > '9')) if (c == EOF) return false; if (c == '-') { n = true; v = 0; } else { n = false; v = c - '0'; } while (c = getchar_buffered() - '0', c >= 0 && c <= 9) v = (v << 3) + (v << 1) + c; if (n) v *= -1; return true; }
-#ifdef __cpp11
 template<typename A, typename ...Args> inline bool RDB(A& a, Args&... rest) { return RDB(a) && RDB(rest...); }
-#else
-template<typename T> inline bool RDB(T& a, T& b) { return RDB(a) && RDB(b); }
-template<typename T> inline bool RDB(T& a, T& b, T& c) { return RDB(a, b) && RDB(c); }
-template<typename T> inline bool RDB(T& a, T& b, T& c, T& d) { return RDB(a, b, c) && RDB(d); }
-template<typename T> inline bool RDB(T& a, T& b, T& c, T& d, T& e) { return RDB(a, b, c, d) && RDB(e); }
-template<typename T> inline bool RDB(T& a, T& b, T& c, T& d, T& e, T& f) { return RDB(a, b, c, d, e) && RDB(f); }
-template<typename T> inline bool RDB(T& a, T& b, T& c, T& d, T& e, T& f, T& g) { return RDB(a, b, c, d, e, f) && RDB(g); }
-template<typename T> inline bool RDB(T& a, T& b, T& c, T& d, T& e, T& f, T& g, T& h) { return RDB(a, b, c, d, e, f, g) && RDB(h); }
-template<typename T> inline bool RDB(T& a, T& b, T& c, T& d, T& e, T& f, T& g, T& h, T& i) { return RDB(a, b, c, d, e, f, g, h) && RDB(i); }
-template<typename T> inline bool RDB(T& a, T& b, T& c, T& d, T& e, T& f, T& g, T& h, T& i, T& j) { return RDB(a, b, c, d, e, f, g, h, i) && RDB(j); }
-#endif
 inline int RDB() { int v; RDB(v); return v; }
+#define RDBn(a, l, r)         { for (int _i_ = l; _i_ < r; ++_i_) RDB((a)[_i_]); }
+#define RDBnm(a, b, c, d, e)  { for (int __i__ = b; __i__ < c; ++__i__) RDBn(a[__i__], d, e) }
+
 // write integer types
 template<typename T> inline void _WR(T a) { if (a < 0) { putchar('-'); a *= -1; } T t = a / 10; if (t) _WR(t); putchar(a - (t << 1) - (t << 3) + '0'); }
 template<typename T> inline void WR_(const T&a) { _WR(a); putchar(' '); }
 template<typename T> inline void WR(const T&a) { _WR(a); putchar('\n'); }
-#ifdef __cpp11
 template<typename A, typename ...Args> inline void _WR(const A& a, const Args&... rest) { WR_(a); _WR(rest...); }
 template<typename A, typename ...Args> inline void WR_(const A& a, const Args&... rest) { WR_(a); WR_(rest...); }
 template<typename A, typename ...Args> inline void WR (const A& a, const Args&... rest) { WR_(a); WR(rest...); }
-#else
-template<typename T> inline void _WR(const T& a, const T& b) { WR_(a); _WR(b); }
-template<typename T> inline void WR_(const T& a, const T& b) { WR_(a); WR_(b); }
-template<typename T> inline void WR (const T& a, const T& b) { WR_(a); WR(b); }
-template<typename T> inline void _WR(const T& a, const T& b, const T& c) { WR_(a, b); _WR(c); }
-template<typename T> inline void WR_(const T& a, const T& b, const T& c) { WR_(a, b); WR_(c); }
-template<typename T> inline void WR (const T& a, const T& b, const T& c) { WR_(a, b); WR(c); }
-template<typename T> inline void _WR(const T& a, const T& b, const T& c, const T& d) { WR_(a, b, c); _WR(d); }
-template<typename T> inline void WR_(const T& a, const T& b, const T& c, const T& d) { WR_(a, b, c); WR_(d); }
-template<typename T> inline void WR (const T& a, const T& b, const T& c, const T& d) { WR_(a, b, c); WR(d); }
-template<typename T> inline void _WR(const T& a, const T& b, const T& c, const T& d, const T& e) { WR_(a, b, c, d); _WR(e); }
-template<typename T> inline void WR_(const T& a, const T& b, const T& c, const T& d, const T& e) { WR_(a, b, c, d); WR_(e); }
-template<typename T> inline void WR (const T& a, const T& b, const T& c, const T& d, const T& e) { WR_(a, b, c, d); WR(e); }
-template<typename T> inline void _WR(const T& a, const T& b, const T& c, const T& d, const T& e, const T& f) { WR_(a, b, c, d, e); _WR(f); }
-template<typename T> inline void WR_(const T& a, const T& b, const T& c, const T& d, const T& e, const T& f) { WR_(a, b, c, d, e); WR_(f); }
-template<typename T> inline void WR (const T& a, const T& b, const T& c, const T& d, const T& e, const T& f) { WR_(a, b, c, d, e); WR(f); }
-template<typename T> inline void _WR(const T& a, const T& b, const T& c, const T& d, const T& e, const T& f, const T& g) { WR_(a, b, c, d, e, f); _WR(g); }
-template<typename T> inline void WR_(const T& a, const T& b, const T& c, const T& d, const T& e, const T& f, const T& g) { WR_(a, b, c, d, e, f); WR_(g); }
-template<typename T> inline void WR (const T& a, const T& b, const T& c, const T& d, const T& e, const T& f, const T& g) { WR_(a, b, c, d, e, f); WR(g); }
-template<typename T> inline void _WR(const T& a, const T& b, const T& c, const T& d, const T& e, const T& f, const T& g, const T& h) { WR_(a, b, c, d, e, f, g); _WR(h); }
-template<typename T> inline void WR_(const T& a, const T& b, const T& c, const T& d, const T& e, const T& f, const T& g, const T& h) { WR_(a, b, c, d, e, f, g); WR_(h); }
-template<typename T> inline void WR (const T& a, const T& b, const T& c, const T& d, const T& e, const T& f, const T& g, const T& h) { WR_(a, b, c, d, e, f, g); WR(h); }
-template<typename T> inline void _WR(const T& a, const T& b, const T& c, const T& d, const T& e, const T& f, const T& g, const T& h, const T& i) { WR_(a, b, c, d, e, f, g, h); _WR(i); }
-template<typename T> inline void WR_(const T& a, const T& b, const T& c, const T& d, const T& e, const T& f, const T& g, const T& h, const T& i) { WR_(a, b, c, d, e, f, g, h); WR_(i); }
-template<typename T> inline void WR (const T& a, const T& b, const T& c, const T& d, const T& e, const T& f, const T& g, const T& h, const T& i) { WR_(a, b, c, d, e, f, g, h); WR(i); }
-template<typename T> inline void _WR(const T& a, const T& b, const T& c, const T& d, const T& e, const T& f, const T& g, const T& h, const T& i, const T& j) { WR_(a, b, c, d, e, f, g, h, i); _WR(j); }
-template<typename T> inline void WR_(const T& a, const T& b, const T& c, const T& d, const T& e, const T& f, const T& g, const T& h, const T& i, const T& j) { WR_(a, b, c, d, e, f, g, h, i); WR_(j); }
-template<typename T> inline void WR (const T& a, const T& b, const T& c, const T& d, const T& e, const T& f, const T& g, const T& h, const T& i, const T& j) { WR_(a, b, c, d, e, f, g, h, i); WR(j); }
-#endif
-#define RDn(a, l, r)          { for (int _i_ = l; _i_ < r; ++_i_) RD((a)[_i_]); }
-#define RDBn(a, l, r)         { for (int _i_ = l; _i_ < r; ++_i_) RDB((a)[_i_]); }
 #define WRn(a, l, r)          { for (int _i_ = l; _i_ < r - 1; ++_i_) WR_((a)[_i_]); if(r > 0) WR((a)[r - 1]); }
-#define RDnm(a, b, c, d, e)   { for (int __i__ = b; __i__ < c; ++__i__) RDn(a[__i__], d, e) }
-#define RDBnm(a, b, c, d, e)  { for (int __i__ = b; __i__ < c; ++__i__) RDBn(a[__i__], d, e) }
 #define WRnm(a, b, c, d, e)   { for (int __i__ = b; __i__ < c; ++__i__) WRn(a[__i__], d, e) }
-
 
 // quick IO for any type using cin, cout
 template <typename A> inline std::istream &IN(A& a) { return std::cin >> a; }
-#ifdef __cpp11
 template <typename A, typename ...Args> inline std::istream& IN(A& a, Args&... rest) { std::cin >> a; return IN(rest...); }
-#else
-template <typename A, typename B> inline std::istream& IN(A& a, B& b) { return std::cin >> a >> b; }
-template <typename A, typename B, typename C> inline std::istream& IN(A& a, B& b, C& c) { return std::cin >> a >> b >> c; }
-template <typename A, typename B, typename C, typename D> inline std::istream& IN(A& a, B& b, C& c, D& d) { return std::cin >> a >> b >> c >> d; }
-template <typename A, typename B, typename C, typename D, typename E> inline std::istream& IN(A& a, B& b, C& c, D& d, E& e) { return std::cin >> a >> b >> c >> d >> e; }
-template <typename A, typename B, typename C, typename D, typename E, typename F> inline std::istream& IN(A& a, B& b, C& c, D& d, E& e, F& f) { return std::cin >> a >> b >> c >> d >> e >> f; }
-template <typename A, typename B, typename C, typename D, typename E, typename F, typename G> inline std::istream& IN(A& a, B& b, C& c, D& d, E& e, F& f, G& g) { return std::cin >> a >> b >> c >> d >> e >> f >> g; }
-template <typename A, typename B, typename C, typename D, typename E, typename F, typename G, typename H> inline std::istream& IN(A& a, B& b, C& c, D& d, E& e, F& f, G& g, H& h) { return std::cin >> a >> b >> c >> d >> e >> f >> g >> h; }
-template <typename A, typename B, typename C, typename D, typename E, typename F, typename G, typename H, typename I> inline std::istream& IN(A& a, B& b, C& c, D& d, E& e, F& f, G& g, H& h, I& i) { return std::cin >> a >> b >> c >> d >> e >> f >> g >> h >> i; }
-template <typename A, typename B, typename C, typename D, typename E, typename F, typename G, typename H, typename I, typename J> inline std::istream& IN(A& a, B& b, C& c, D& d, E& e, F& f, G& g, H& h, I& i, J& j) { return std::cin >> a >> b >> c >> d >> e >> f >> g >> h >> i >> j; }
-#endif
+#define INn(a, l, r)          { for (int _i_ = l; _i_ < r; ++_i_) IN(a[_i_]); }
+#define INnm(a, b, c, d, e)   { for (int __i__ = b; __i__ < c; ++__i__) INn(a[__i__], d, e) }
+
 inline std::ostream& OUT() { return std::cout << std::endl; }
-#ifdef __cpp11
 template <typename A> inline std::ostream& _OUT(const A &a) { return std::cout << a; }
 template <typename A, typename...Args> inline std::ostream& _OUT(const A &a, const Args&... rest) { std::cout << a << " "; return _OUT(rest...); }
 template <typename...Args> inline std::ostream& OUT_(const Args&... args) { _OUT(args...); return std::cout << " "; }
 template <typename...Args> inline std::ostream& OUT(const Args&... args) { _OUT(args...); return std::cout << std::endl; }
-#else
-template<typename T> inline std::ostream& _OUT(const T&a) { return std::cout << a; }
-template<typename T> inline std::ostream& OUT_(const T&a) { return std::cout << a << ' '; }
-template<typename T> inline std::ostream& OUT(const T&a) { return std::cout << a << std::endl; }
-template<typename T, typename U> inline std::ostream& _OUT(const T&a, const U&b) { return std::cout << a << " " << b; }
-template<typename T, typename U> inline std::ostream& OUT_(const T&a, const U&b) { return std::cout << a << " " << b << " "; }
-template<typename T, typename U> inline std::ostream& OUT(const T&a, const U&b) { return std::cout << a << " " << b << std::endl; }
-template<typename T, typename U, typename V> inline std::ostream& _OUT(const T&a, const U&b, const V&c) { OUT_(a, b); return _OUT(c); }
-template<typename T, typename U, typename V> inline std::ostream& OUT_(const T&a, const U&b, const V&c) { OUT_(a, b); return OUT_(c); }
-template<typename T, typename U, typename V> inline std::ostream& OUT(const T&a, const U&b, const V&c) { OUT_(a, b); return OUT(c); }
-template<typename T, typename U, typename V, typename W> inline std::ostream& _OUT(const T&a, const U&b, const V&c, const W&d) { OUT_(a, b, c); return _OUT(d); }
-template<typename T, typename U, typename V, typename W> inline std::ostream& OUT_(const T&a, const U&b, const V&c, const W&d) { OUT_(a, b, c); return OUT_(d); }
-template<typename T, typename U, typename V, typename W> inline std::ostream& OUT(const T&a, const U&b, const V&c, const W&d) { OUT_(a, b, c); return OUT(d); }
-template<typename T, typename U, typename V, typename W, typename X> inline std::ostream& _OUT(const T&a, const U&b, const V&c, const W&d, const X&e) { OUT_(a, b, c, d); return _OUT(e); }
-template<typename T, typename U, typename V, typename W, typename X> inline std::ostream& OUT_(const T&a, const U&b, const V&c, const W&d, const X&e) { OUT_(a, b, c, d); return OUT_(e); }
-template<typename T, typename U, typename V, typename W, typename X> inline std::ostream& OUT(const T&a, const U&b, const V&c, const W&d, const X&e) { OUT_(a, b, c, d); return OUT(e); }
-template<typename T, typename U, typename V, typename W, typename X, typename Y> inline std::ostream& _OUT(const T&a, const U&b, const V&c, const W&d, const X&e, const Y&f) { OUT_(a, b, c, d, e); return _OUT(f); }
-template<typename T, typename U, typename V, typename W, typename X, typename Y> inline std::ostream& OUT_(const T&a, const U&b, const V&c, const W&d, const X&e, const Y&f) { OUT_(a, b, c, d, e); return OUT_(f); }
-template<typename T, typename U, typename V, typename W, typename X, typename Y> inline std::ostream& OUT(const T&a, const U&b, const V&c, const W&d, const X&e, const Y&f) { OUT_(a, b, c, d, e); return OUT(f); }
-template<typename T, typename U, typename V, typename W, typename X, typename Y, typename Z> inline std::ostream& _OUT(const T&a, const U&b, const V&c, const W&d, const X&e, const Y&f, const Z&g) { OUT_(a, b, c, d, e, f); return _OUT(g); }
-template<typename T, typename U, typename V, typename W, typename X, typename Y, typename Z> inline std::ostream& OUT_(const T&a, const U&b, const V&c, const W&d, const X&e, const Y&f, const Z&g) { OUT_(a, b, c, d, e, f); return OUT_(g); }
-template<typename T, typename U, typename V, typename W, typename X, typename Y, typename Z> inline std::ostream& OUT(const T&a, const U&b, const V&c, const W&d, const X&e, const Y&f, const Z&g) { OUT_(a, b, c, d, e, f); return OUT(g); }
-template<typename T, typename U, typename V, typename W, typename X, typename Y, typename Z, typename A> inline std::ostream& _OUT(const T&a, const U&b, const V&c, const W&d, const X&e, const Y&f, const Z&g, const A&h) { OUT_(a, b, c, d, e, f, g); return _OUT(h); }
-template<typename T, typename U, typename V, typename W, typename X, typename Y, typename Z, typename A> inline std::ostream& OUT_(const T&a, const U&b, const V&c, const W&d, const X&e, const Y&f, const Z&g, const A&h) { OUT_(a, b, c, d, e, f, g); return OUT_(h); }
-template<typename T, typename U, typename V, typename W, typename X, typename Y, typename Z, typename A> inline std::ostream& OUT(const T&a, const U&b, const V&c, const W&d, const X&e, const Y&f, const Z&g, const A&h) { OUT_(a, b, c, d, e, f, g); return OUT(h); }
-template<typename T, typename U, typename V, typename W, typename X, typename Y, typename Z, typename A, typename B> inline std::ostream& _OUT(const T&a, const U&b, const V&c, const W&d, const X&e, const Y&f, const Z&g, const A&h, const B&i) { OUT_(a, b, c, d, e, f, g, h); return _OUT(i); }
-template<typename T, typename U, typename V, typename W, typename X, typename Y, typename Z, typename A, typename B> inline std::ostream& OUT_(const T&a, const U&b, const V&c, const W&d, const X&e, const Y&f, const Z&g, const A&h, const B&i) { OUT_(a, b, c, d, e, f, g, h); return OUT_(i); }
-template<typename T, typename U, typename V, typename W, typename X, typename Y, typename Z, typename A, typename B> inline std::ostream& OUT(const T&a, const U&b, const V&c, const W&d, const X&e, const Y&f, const Z&g, const A&h, const B&i) { OUT_(a, b, c, d, e, f, g, h); return OUT(i); }
-template<typename T, typename U, typename V, typename W, typename X, typename Y, typename Z, typename A, typename B, typename C> inline std::ostream& _OUT(const T&a, const U&b, const V&c, const W&d, const X&e, const Y&f, const Z&g, const A&h, const B&i, const C&j) { OUT_(a, b, c, d, e, f, g, h, i); return _OUT(j); }
-template<typename T, typename U, typename V, typename W, typename X, typename Y, typename Z, typename A, typename B, typename C> inline std::ostream& OUT_(const T&a, const U&b, const V&c, const W&d, const X&e, const Y&f, const Z&g, const A&h, const B&i, const C&j) { OUT_(a, b, c, d, e, f, g, h, i); return OUT_(j); }
-template<typename T, typename U, typename V, typename W, typename X, typename Y, typename Z, typename A, typename B, typename C> inline std::ostream& OUT(const T&a, const U&b, const V&c, const W&d, const X&e, const Y&f, const Z&g, const A&h, const B&i, const C&j) { OUT_(a, b, c, d, e, f, g, h, i); return OUT(j); }
-#endif
-#define INn(a, l, r)          { for (int _i_ = l; _i_ < r; ++_i_) IN(a[_i_]); }
-#define INnm(a, b, c, d, e)   { for (int __i__ = b; __i__ < c; ++__i__) INn(a[__i__], d, e) }
 #define OUTn(a, l, r)         { for (int _i_ = l; _i_ < r - 1; ++_i_) OUT_((a)[_i_]); if(r > 0) OUT((a)[r - 1]); }
 #define OUTnm(a, b, c, d, e)  { for (int __i__ = b; __i__ < c; ++__i__) OUTn(a[__i__], d, e) }
 
