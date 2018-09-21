7ed894714e215bdce8f236fcb300e960a85459fc
lishuangquan <lishuangquan@bytedance.com>
2018-09-21 18:11:05 +0800

    geometry: implement new point
    Change-Id: I38285733942f982b9d6e99c6a3d61818935f0d87

--- geometry/point.h
+++ geometry/point.h
@@ -14,49 +14,58 @@ namespace cil {
 
 // point in 2D plane
 template<typename T>
-struct point {
-    T x, y;
-    point() {}
-    point(T xx, T yy) : x(xx), y(yy) {}
-    point(const point& B) : x(B.x), y(B.y) {}
-    double radius() const { return sqrt(x * x + y *y); }
-    double angle() const { return atan2(y, x); }
-    double angle_degree() const { return atan2(y, x) * 180 / 3.14159265358979323846264338327950288419716939937510; }
-    bool operator == (const point& B) const { return x == B.x && y == B.y; }
-    bool operator != (const point& B) const { return x != B.x || y != B.y; }
-    bool operator < (const point& B) const { return x < B.x || (x == B.x && y < B.y); }
-    bool operator > (const point& B) const { return x > B.x || (x == B.x && y > B.y); }
-    bool operator <= (const point& B) const { return x < B.x || (x == B.x && y <= B.y); }
-    bool operator >= (const point& B) const { return x > B.x || (x == B.x && y >= B.y); }
-    point& operator = (const point& B) { x = B.x; y = B.y; return *this; }
-    point operator + (const point& B) const { return point(x + B.x, y + B.y); }
-    point operator - (const point& B) const { return point(x - B.x, y - B.y); }
-    point& operator += (const point& B) { x += B.x; y += B.y; return *this; }
-    point& operator -= (const point& B) { x -= B.x; y -= B.y; return *this; }
-    T operator * (const point& B) const { return x * B.x + y * B.y; }
-    T operator ^ (const point& B) const { return x * B.y - y * B.x; }
+class point {
+    T __x, __y;
+
+public:
+    point(): __x(0), __y(0) {}
+    point(T x, T y) : __x(x), __y(y) {}
+    point(const point& p) : __x(p.__x), __y(p.__y) {}
+
+    T x() const { return __x; }
+    T y() const { return __y; }
+
+    double radius() const { return sqrt(__x * __x + __y *__y); }
+    double angle() const { return atan2(__y, __x); }
+    double angle_degree() const { return atan2(__y, __x) * 180 / 3.14159265358979323846264338327950288419716939937510; }
+
+    bool operator == (const point& B) const { return __x == B.__x && __y == B.__y; }
+    bool operator != (const point& B) const { return __x != B.__x || __y != B.__y; }
+    bool operator < (const point& B) const { return __x < B.__x || (__x == B.__x && __y < B.__y); }
+    bool operator > (const point& B) const { return __x > B.__x || (__x == B.__x && __y > B.__y); }
+    bool operator <= (const point& B) const { return __x < B.__x || (__x == B.__x && __y <= B.__y); }
+    bool operator >= (const point& B) const { return __x > B.__x || (__x == B.__x && __y >= B.__y); }
+
+    point& operator = (const point& B) { __x = B.__x; __y = B.__y; return *this; }
+    point operator + (const point& B) const { return point(__x + B.__x, __y + B.__y); }
+    point operator - (const point& B) const { return point(__x - B.__x, __y - B.__y); }
+    point& operator += (const point& B) { __x += B.__x; __y += B.__y; return *this; }
+    point& operator -= (const point& B) { __x -= B.__x; __y -= B.__y; return *this; }
+    T operator * (const point& B) const { return __x * B.__x + __y * B.__y; }
+    T operator ^ (const point& B) const { return __x * B.__y - __y * B.__x; }
 };
 
 template<typename T> std::ostream& operator<< (std::ostream& os, const point<T>& p) {
-    return os << "(" << p.x << ", " << p.y << ")" << std::flush;
+    return os << "(" << p.x() << ", " << p.y() << ")" << std::flush;
 }
 
 namespace std {
 template<typename T>
 struct hash<point<T> > {
-    size_t operator()(const point<T> &p) const {
-        return std::hash<T>{}((std::hash<T>{}(p.x) * 19921211) ^ (std::hash<T>{}(p.y) * 2147483647));
+    size_t operator()(const point<T>& p) const {
+        return std::hash<T>{}((std::hash<T>{}(p.x()) * 19921211) ^ (std::hash<T>{}(p.y()) * 2147483647));
     }
 };
-}
+} // namespace std
 
 template<typename T> double distance(const point<T>& A, const point<T>& B) {
-    return sqrt((A.x - B.x) * (A.x - B.x) + (A.y - B.y) * (A.y - B.y));
+    return sqrt((A.x() - B.x()) * (A.x() - B.x()) + (A.y() - B.y()) * (A.y() - B.y()));
 }
+
 template<typename T> double distance(T x1, T y1, T x2, T y2) {
     return sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
 }
 
-}
+} // namespace cil
 
 #endif //__GEOMETRY_POINT_H__
