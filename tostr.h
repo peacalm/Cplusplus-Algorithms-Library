ae85459b1f8d40c4f0e507cd29f8b825f6411a79
lishuangquan <lishuangquan@bytedance.com>
2019-12-26 21:04:54 +0800

    tostr: add more containers
    Change-Id: I3c0d00bcff54d8e13bf18a5edf22c62e1bde4787

--- tostr.h
+++ tostr.h
@@ -1,10 +1,109 @@
 #pragma once
 
+#include <array>
+#include <deque>
+#include <forward_list>
+#include <initializer_list>
+#include <list>
+#include <map>
+#include <set>
 #include <string>
 #include <sstream>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
 #include <vector>
 
 namespace cil {
+
+// =============================================================================
+// declaration
+// -----------------------------------------------------------------------------
+
+template <typename T>
+std::string tostr(const T& t);
+
+template <typename T, typename U>
+std::string tostr(const std::pair<T, U>& t);
+
+template <typename T, size_t N>
+std::string tostr(const std::array<T, N>& t);
+
+template <typename T>
+std::string tostr(const std::deque<T>& t);
+
+template <typename T>
+std::string tostr(const std::forward_list<T>& t);
+
+template <typename T>
+std::string tostr(const std::initializer_list<T>& t);
+
+template <typename T>
+std::string tostr(const std::list<T>& t);
+
+template <typename T, typename U>
+std::string tostr(const std::map<T, U>& t);
+
+template <typename T>
+std::string tostr(const std::set<T>& t);
+
+template <typename T, typename U>
+std::string tostr(const std::unordered_map<T, U>& t);
+
+template <typename T>
+std::string tostr(const std::unordered_set<T>& t);
+
+template <typename T>
+std::string tostr(const std::vector<T>& t);
+
+
+
+template <typename T, typename ...Args>
+void push_to_str_vec(std::vector<std::string>& strs, const T& t, const Args&... args);
+
+template <typename T, typename ...Args>
+std::vector<std::string> tostrs(const T& t, const Args&... args);
+
+template <typename ...Args>
+std::string tostr_with_delim(const std::string& delim, const Args&... args);
+
+template <typename ...Args>
+std::string tostr(const Args&... args);
+
+
+// =============================================================================
+// implementations
+// -----------------------------------------------------------------------------
+
+template <typename ForwardIterator>
+std::string __item_single_container_to_str(
+        ForwardIterator b, ForwardIterator e,
+        const std::string& l, const std::string& r, const std::string& d) {
+    std::string ret{l};
+    ForwardIterator it = b;
+    while (it != e) {
+        if (it != b) ret += d;
+        ret += tostr(*it);
+        ++it;
+    }
+    ret += r;
+    return ret;
+}
+
+template <typename ForwardIterator>
+std::string __item_pair_container_to_str(
+        ForwardIterator b, ForwardIterator e,
+        const std::string& l, const std::string& r, const std::string& d) {
+    std::string ret{l};
+    ForwardIterator it = b;
+    while (it != e) {
+        if (it != b) ret += d;
+        ret += tostr(it->first) + ": " + tostr(it->second);
+        ++it;
+    }
+    ret += r;
+    return ret;
+}
 
 template <typename T>
 std::string tostr(const T& t) {
@@ -12,6 +111,62 @@ std::string tostr(const T& t) {
     os << t;
     return os.str();
 }
+
+template <typename T, typename U>
+std::string tostr(const std::pair<T, U>& t) {
+    return "(" + tostr(t.first) + ", " + tostr(t.second) + ")";
+};
+
+template <typename T, size_t N>
+std::string tostr(const std::array<T, N>& t) {
+    return __item_single_container_to_str(t.begin(), t.end(), "[", "]", ", ");
+};
+
+template <typename T>
+std::string tostr(const std::deque<T>& t) {
+    return __item_single_container_to_str(t.begin(), t.end(), "[", "]", ", ");
+};
+
+template <typename T>
+std::string tostr(const std::forward_list<T>& t) {
+    return __item_single_container_to_str(t.begin(), t.end(), "[", "]", ", ");
+};
+
+template <typename T>
+std::string tostr(const std::initializer_list<T>& t) {
+    return __item_single_container_to_str(t.begin(), t.end(), "[", "]", ", ");
+};
+
+template <typename T>
+std::string tostr(const std::list<T>& t) {
+    return __item_single_container_to_str(t.begin(), t.end(), "[", "]", ", ");
+};
+
+template <typename T, typename U>
+std::string tostr(const std::map<T, U>& t) {
+    return __item_pair_container_to_str(t.begin(), t.end(), "{", "}", ", ");
+}
+
+template <typename T>
+std::string tostr(const std::set<T>& t) {
+    return __item_single_container_to_str(t.begin(), t.end(), "{", "}", ", ");
+}
+
+template <typename T, typename U>
+std::string tostr(const std::unordered_map<T, U>& t) {
+    return __item_pair_container_to_str(t.begin(), t.end(), "{", "}", ", ");
+}
+
+template <typename T>
+std::string tostr(const std::unordered_set<T>& t) {
+    return __item_single_container_to_str(t.begin(), t.end(), "{", "}", ", ");
+}
+
+template <typename T>
+std::string tostr(const std::vector<T>& t) {
+    return __item_single_container_to_str(t.begin(), t.end(), "[", "]", ", ");
+}
+
 
 template <typename T>
 void push_to_str_vec(std::vector<std::string>& strs, const T& t) {
@@ -44,7 +199,8 @@ std::string tostr_with_delim(const std::string& delim, const Args&... args) {
 
 template <typename ...Args>
 std::string tostr(const Args&... args) {
-    return tostr_with_delim(" ", args...);
+    return tostr_with_delim(", ", args...);
 };
+
 
 }  // namespace cil
